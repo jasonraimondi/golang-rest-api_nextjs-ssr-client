37a9ad88129dd329659c18efc76a0879c2411f0f
Jason Raimondi <jason@raimondi.us>
2019-06-12 18:20:32 -0700

    Comment out unused code to have tests pass for CI.

--- db/seed.go
+++ db/seed.go
@@ -1,22 +1,18 @@
 package db
 
-import (
-	"git.jasonraimondi.com/jason/jasontest/action"
-	"git.jasonraimondi.com/jason/jasontest/lib"
-)
-
-type SeedData struct {
-	a *lib.Application
-}
-
-func (s *SeedData) Seed() error {
-	//s.people()
-	return nil
-}
-
-func (s *SeedData) people() error {
-	var commands []interface{}
-	commands = append(commands, action.CreatePerson{})
-	return s.a.Dispatch(commands)
-	return nil
-}
+//
+//type SeedData struct {
+//	a *lib.Application
+//}
+//
+//func (s *SeedData) Seed() error {
+//	//s.people()
+//	return nil
+//}
+//
+//func (s *SeedData) people() error {
+//	var commands []interface{}
+//	commands = append(commands, action.CreatePerson{})
+//	return s.a.Dispatch(commands)
+//	return nil
+//}
