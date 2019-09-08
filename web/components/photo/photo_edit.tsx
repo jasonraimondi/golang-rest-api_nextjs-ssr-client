99e14e86c276c90c300604380421df3a3e421d44
Jason Raimondi <jason@raimondi.us>
2019-09-08 15:09:40 -0700

    Refactor photo edit.

--- web/components/photo/photo_edit.tsx
+++ web/components/photo/photo_edit.tsx
@@ -1,9 +1,12 @@
 import { Formik, FormikActions, FormikProps } from "formik";
-import React from "react";
-import { updatePhoto } from "../../lib/api/photos";
+import React, { useState } from "react";
+import { removeTagFromPhoto, updatePhoto } from "../../lib/api/photos";
+import { Photo } from "../../lib/entity/photo";
+import { AuthToken } from "../../lib/services/auth_token";
 
 import { SubmitButton } from "../forms/button";
 import { TextInput } from "../forms/text";
+import { Tag } from "../tag";
 
 type PhotoInputs = {
   tags: string;
@@ -11,88 +14,103 @@ type PhotoInputs = {
   description: string;
 }
 
-type Props = PhotoInputs & {
-  photoId: string;
+type Props = {
+  auth: AuthToken
+  photo: Photo;
   afterSave(): void;
 };
 
-export const EditPhoto = ({ photoId, afterSave, tags, description, app }: Props) => {
+export const EditPhoto = ({ photo, auth, afterSave }: Props) => {
+
   const initialValues = {
-    tags,
-    description,
-    app,
+    description: photo.Description.String,
+    app: photo.App ? photo.App.Name : "",
+    tags: "",
   };
 
   const validate = (values: PhotoInputs) => {
     let errors: Partial<PhotoInputs> = {};
-    if (!values.tags) {
-      errors.tags = "Required";
-    } else if (!values.description) {
-      errors.description = `Required`;
-    }
-
+    // if (!values.tags) {
+    //   errors.tags = "Required";
+    // } else if (!values.description) {
+    //   errors.description = `Required`;
+    // }
+    console.log(values);
     return errors;
   };
 
   const onSubmit = async (values: PhotoInputs, { setSubmitting, setStatus }: FormikActions<PhotoInputs>) => {
     const tags = values.tags.split(", ");
-    // @TODO fix auth string...
-    const errorMessage: string | null = await updatePhoto("authstring", photoId, tags, values.description, values.app);
+    const errorMessage: string | null = await updatePhoto(auth.authorizationString, photo.ID, tags, values.description, values.app);
     if (errorMessage) setStatus(errorMessage);
     setSubmitting(false);
     afterSave();
   };
 
-  return <Formik
-    initialValues={initialValues}
-    validate={validate}
-    onSubmit={onSubmit}
-  >
-    {({
-      values,
-      status,
-      errors,
-      touched,
-      handleChange,
-      handleBlur,
-      handleSubmit,
-      isSubmitting,
-    }: FormikProps<PhotoInputs>) => <form onSubmit={handleSubmit}>
-      <p>{status ? status : null}</p>
-      <TextInput type="text"
-                 label={"Description"}
-                 name={"description"}
-                 touched={touched.description}
-                 value={values.description}
-                 error={errors.description}
-                 handleBlur={handleBlur}
-                 handleChange={handleChange}
-                 submitting={isSubmitting}
-                 required
-      />
-      <TextInput type="text"
-                 label={"App"}
-                 name={"app"}
-                 touched={touched.app}
-                 value={values.app}
-                 error={errors.app}
-                 handleBlur={handleBlur}
-                 handleChange={handleChange}
-                 submitting={isSubmitting}
-                 required
-      />
-      <TextInput type="text"
-                 label={"Add Tags"}
-                 name={"tags"}
-                 touched={touched.tags}
-                 value={values.tags}
-                 error={errors.tags}
-                 handleBlur={handleBlur}
-                 handleChange={handleChange}
-                 submitting={isSubmitting}
-                 required
-      />
-      <SubmitButton label="Submit" type="submit" disabled={isSubmitting}/>
-    </form>}
-  </Formik>;
+  const [tags, setTags] = useState(photo.Tags);
+
+  const handleRemoveTag = async (photoId: string, tagId: number) => {
+    const res: any = await removeTagFromPhoto(auth.authorizationString, photoId, tagId);
+    if (res.status == 202) {
+      setTags(tags.filter(tag => tag.ID !== tagId));
+    }
+  };
+
+  return <>
+    <p className="pt-4"><strong>Tags:</strong><br /> {tags.length ? tags.map(tag => {
+      return <Tag tag={tag} handleRemoveTag={() => handleRemoveTag(photo.ID, tag.ID)} key={tag.ID}/>;
+    }) : "no tags"}</p>
+    <Formik
+      initialValues={initialValues}
+      validate={validate}
+      onSubmit={onSubmit}
+    >
+      {({
+        values,
+        status,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }: FormikProps<PhotoInputs>) => <form onSubmit={handleSubmit}>
+        <p>{status ? status : null}</p>
+        <TextInput type="text"
+                   label={"Description"}
+                   name={"description"}
+                   touched={touched.description}
+                   value={values.description}
+                   error={errors.description}
+                   handleBlur={handleBlur}
+                   handleChange={handleChange}
+                   submitting={isSubmitting}
+                   required
+        />
+        <TextInput type="text"
+                   label={"App"}
+                   name={"app"}
+                   touched={touched.app}
+                   value={values.app}
+                   error={errors.app}
+                   handleBlur={handleBlur}
+                   handleChange={handleChange}
+                   submitting={isSubmitting}
+                   required
+        />
+        <TextInput type="text"
+                   label={"Add Tags"}
+                   name={"tags"}
+                   touched={touched.tags}
+                   value={values.tags}
+                   error={errors.tags}
+                   handleBlur={handleBlur}
+                   handleChange={handleChange}
+                   submitting={isSubmitting}
+                   required
+        />
+        <SubmitButton label="Submit" type="submit" disabled={isSubmitting}/>
+      </form>}
+    </Formik>
+    </>;
 };
--- web/pages/admin/photos/[id].tsx
+++ web/pages/admin/photos/[id].tsx
@@ -1,46 +1,37 @@
 import { NextPage, NextPageContext } from "next";
 import Router from "next/router";
-import React, { useState } from "react";
+import React from "react";
+
+import { adminLayout } from "../../../components/admin/admin_layout";
 import { AuthProps } from "../../../components/auth/private_route";
 import { TextInput } from "../../../components/forms/text";
-import { adminLayout } from "../../../components/admin/admin_layout";
 import { EditPhoto } from "../../../components/photo/photo_edit";
-import { Tag } from "../../../components/tag";
+import { getPhoto, PHOTO_BASE_PATH } from "../../../lib/api/photos";
 import { Photo } from "../../../lib/entity/photo";
 import { APP_ROUTES } from "../../../lib/routes";
-import { getPhoto, PHOTO_BASE_PATH, removeTagFromPhoto } from "../../../lib/api/photos";
 
 type Props = AuthProps & {
   photo: Photo,
 };
 
 const Page: NextPage<Props> = ({ photo, auth }) => {
-  const [tags, setTags] = useState(photo.Tags);
 
-  const handleRemoveTag = async (photoId: string, tagId: number) => {
-    console.log(auth, photoId, tagId);
-    const res: any = await removeTagFromPhoto("auth.authorizationString", photoId, tagId);
-    if (res.status == 202) {
-      setTags(tags.filter(tag => tag.ID !== tagId));
-    }
+  const afterSave = async () => {
+    await Router.push(APP_ROUTES.admin.photos.show.create({ photoId: photo.ID }))
   };
 
-  const tagList = tags.length ? tags.map(tag => {
-    return <Tag tag={tag} handleRemoveTag={() => handleRemoveTag(photo.ID, tag.ID)} key={tag.ID}/>;
-  }) : "no tags";
-
   return <div className="container mx-auto max-w-sm">
-    <img width={420} src={PHOTO_BASE_PATH + photo.RelativeURL} alt={photo.Description.String}
-         title={photo.Description.String}/>
+    <img width={420}
+         src={PHOTO_BASE_PATH + photo.RelativeURL}
+         alt={photo.Description.String}
+         title={photo.Description.String}
+    />
     <TextInput type="text" label="File Size" name="file-size" value={photo.FileSizeHuman} disabled={true}/>
     <TextInput type="text" label="Mime Type" name="mine-type" value={photo.MimeType} disabled={true}/>
     <TextInput type="text" label="File Name" name="file-size" value={photo.FileName} disabled={true}/>
-    <p className="pt-4"><strong>Tags:</strong><br /> {tagList}</p>
-    <EditPhoto photoId={photo.ID}
-               app={photo.App ? photo.App.Name : ""}
-               tags={""}
-               description={photo.Description ? photo.Description.String : ""}
-               afterSave={() => Router.push(APP_ROUTES.admin.photos.show.create({ photoId: photo.ID }))}
+    <EditPhoto auth={auth}
+               photo={photo}
+               afterSave={afterSave}
     />
   </div>;
 };
